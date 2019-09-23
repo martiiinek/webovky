8803dae0ad809570d57ed27a70ad719a9e0c0ba7
melisik <melisik@porg.cz>
2019-09-23 17:42:44 +0200

    Webovy servrik

--- webovky1.js
+++ webovky1.js
@@ -5,9 +5,23 @@ let citac = 0;
 http.createServer((req, res) => {
     if (req.url == "/"){
     citac++; //dtoo citac=citac+1
-        0}
+        }
+    if (req.url == "/jinastranka") {
+        res.writeHead(200, {"Content-type": "text/html"});
+        res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Nečum</body></html>");
+    }
+        else if (req.url == "/jsondemo")  {
+            res.writeHead(200, {"Contennt-type": "application/json"});
+            let obj = {};
+            obj.jmeno = "BoB";
+            obj.prijmeni = "Ross";
+            obj.rokNarozeni = 2002;
+            res.end(JSON.stringify(obj));
+}
+     else{
     res.writeHead(200, {"Content-type": "text/html"});
     res.end("<html lang='cs'><head><meta charset='UTF8'></head><body>Počet volání: " +citac +"</body></html>");
+        }
 }).listen(8888);
 
 
